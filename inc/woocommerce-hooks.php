fa2fb6a64b084f7d2041ca7fda3f60af986897ba
Roman Andrushchenko <roman44work@gmail.com>
2023-07-20 10:29:41 +0300

    010. Cart product (part 1)

--- inc/woocommerce-hooks.php
+++ inc/woocommerce-hooks.php
@@ -20,3 +20,10 @@ add_action ('woocommerce_shop_loop_subcategory_title', function ($category) {
     echo "<h6>". esc_html ($category -> name). "</h6>";
     echo '<small class="text-body">'.$category -> count. __(' Products', 'woostudy').'</small>';
 });
+
+
+//Ссылка на товар в карточке товара
+remove_action ('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+remove_action ('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
+
+remove_action ('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
